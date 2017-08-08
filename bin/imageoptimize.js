83186f426cb808ada698a543a5dc8b2caee9f980
otss <zhangxu_wo@163.com>
2017-08-08 10:41:31 +0800

    update

--- bin/imageoptimize.js
+++ bin/imageoptimize.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 const pkg = require('../package.json');
 const commandOptions = require('../lib/cli/options.json');
 
@@ -6,13 +7,13 @@ const commandOptions = require('../lib/cli/options.json');
 if (!module.parent) {
 	let argv = require('yargs')
 		.usage('Usage: imageoptimize [options]')
-		.options(commandOptions.imagemin)
+		.options(commandOptions.imageoptimize)
 		.demandOption('i')
 		.example('Usage: imageoptimize -i /abc/edf -o ../xyz')
 		.help()
 		.version(() => pkg.version)
-		.epilogue("查看使用文档，输入 imagemin --help")
+		.epilogue('查看使用文档，输入 imageoptimize --help')
 		.argv;
-	
+
 	require('../lib/cli')(argv);
 }
